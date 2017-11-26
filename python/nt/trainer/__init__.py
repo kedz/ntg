4f168af7c56c34cff5ff001abf284dd2859a6b08
kedz <kedzie@cs.columbia.edu>
2017-11-26 17:04:39 -0500

    Imported trainer_core methods.

--- python/nt/trainer/__init__.py
+++ python/nt/trainer/__init__.py
@@ -1,2 +1,3 @@
 from .trainer_helper import (generate_splits, stratified_generate_splits,
                              kfold_iter, stratified_kfold_iter)
+from .trainer_core import (optimize_criterion, train_epoch, eval)
