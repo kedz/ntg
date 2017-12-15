d96b5ffc730b3d4ff45e8f38b0bcbf387f516f13
Chris Kedzie <kedzie@cs.columbia.edu>
2017-12-14 20:51:39 -0800

    Added download to buffer method.

--- python/ntp/datasets/util.py
+++ python/ntp/datasets/util.py
@@ -1,4 +1,8 @@
+import sys
 import os
+import urllib.request
+import io
+
 
 def get_data_dir():
     '''
@@ -13,3 +17,18 @@ def get_data_dir():
         os.mkdir(data_dir)
     return data_dir
 
+def download_url_to_buffer(url, chunk_size=5096):
+    response = urllib.request.urlopen(url)
+    size = int(response.headers['content-length'])
+    read = 0
+    buffer = io.BytesIO()
+    while read < size:
+        chunk = response.read(chunk_size)
+        read += len(chunk)
+        buffer.write(chunk)
+        sys.stdout.write("\rread {}/{} bytes ({:0.3f}%)".format(
+            read, size, read / size * 100))
+        sys.stdout.flush()
+    print("")
+    buffer.seek(0)
+    return buffer
